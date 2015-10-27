2d50d3ed40bf38d81673732a9b31727fbe580fc0
Aakash Kash Shah ahs2hd <ahs2hd@virginia.edu>
2015-10-26 23:58:57 -0400

    More fixing
    Corrected create_transaction, create_user, and lookup_user,

--- refer_exp/refer_exp/exp.py
+++ refer_exp/refer_exp/exp.py
@@ -27,7 +27,6 @@ from django.http import JsonResponse
 from django import db
 from main import models #TODO: import specific models from .models, to prevent having to retype models everytime
 from django.core import serializers
-
 from stuff import main
 
 
@@ -35,13 +34,19 @@ from stuff import main
 #Returns a JSON containing username, id, and company.
 def all_sellers(request):
   # r = requests.get(r'^localhost:8001/api/v1/sellers/all$') 
-  return main.all_sellers(request)
+  req = urllib.request.Request(r'^localhost:8001/api/v1/buyers/all$')
+  resp_json = urllib.request.urlopen(req).read().decode('utf-8')  
+  resp = json.loads(resp_json)
+  return resp
 
 #GET request from HTML to ModelAPI
 #Returns a JSON containing username, id, and company.
 def all_buyers(request):
   # r = requests.get(r'^localhost:8001/api/v1/buyers/all$') 
-  return main.all_buyers(request)
+  req = urllib.request.Request(r'^localhost:8001/api/v1/sellers/all$')
+  resp_json = urllib.request.urlopen(req).read().decode('utf-8')  
+  resp = json.loads(resp_json)
+  return resp
 
 #POST request from HTML to ModelAPI
 #requies a username and password field
@@ -71,18 +76,17 @@ def log_in(request, username, password):
 def log_out(auth):
   if request.method != 'POST':
         return _error_response(request, "must make POST request")
+  if 'authenticator' not in request.POST:
+         return _error_response(request, "missing auth")
   post_data = {
   "authenticator" : request.POST['authenticator']}
   } 
-  if 'authenticator' not in request.POST:
-        return _error_response(request, "missing auth")
   post_encoded = urllib.parse.urlencode(post_data).encode('utf-8')
-  req = urllib.request.Request(r'^localhost:8001/api/v1/auth/login$', data=post_encoded, method='POST')
+  req = urllib.request.Request(r'^localhost:8001/api/v1/auth/logout$', data=post_encoded, method='POST')
   resp_json = urllib.request.urlopen(req).read().decode('utf-8')
   resp = json.loads(resp_json)
   return JsonResponse(resp)
   '''
-
   info = {
   'authenticator':auth
   }
@@ -96,7 +100,21 @@ def log_out(auth):
 #need to be valid IDs
 #returns a transaction_id if valid
 def create_transaction(request):
-  return main.create_transaction(request)
+  if request.method != 'POST':
+        return _error_response(request, "must make POST request")
+  if 'buyer_u_id' not in request.POST or 'seller_u_id' not in request.POST or 'negotiation' not in request.POST:
+        return _error_response(request, "missing required fields")
+  post_data = {
+  "buyer_u_id" : request.POST['buyer_u_id']}
+  "seller_u_id" : request.POST['seller_u_id']}
+  "negotiation" : request.POST['negotiation']}
+  } 
+  post_encoded = urllib.parse.urlencode(post_data).encode('utf-8')
+  req = urllib.request.Request(r'^localhost:8001/api/v1/transaction/create$', data=post_encoded, method='POST')
+  resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+  resp = json.loads(resp_json)
+  return JsonResponse(resp)
+
 
 #GET request
 #user_id field
@@ -104,14 +122,21 @@ def create_transaction(request):
 def lookup_user(request, user_id):
   if  request.method !='GET':
         return _error_response(request, "must make GET request")
+  param_encoded = urllib.parse.urlencode(request.GET.get('user_id','')).encode('utf-8')  
+  req = urllib.request.Request(r'^localhost:8001/api/v1/buyers/all$'+param_encoded)
+  resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+  resp = json.loads(resp_json)
+  return JsonResponse(resp)
+  '''
   args = user_id
   r = requests.get(r'^localhost:8001/api/v1/users/(\d+)$')
   return main.lookup_user(r,args)
-
+  '''
+'''
 def view_all_transactions():
   r = requests.get(r'^localhost:8001/api/v1/transaction/all$') 
   return main.view_all_transactions(r)
-
+'''
 
 #POST
 #usertype, password fields
@@ -129,7 +154,7 @@ def create_user(request):
   "password":request.POST['password']
   } 
   post_encoded = urllib.parse.urlencode(post_data).encode('utf-8')
-  req = urllib.request.Request(r'^localhost:8001/api/v1/auth/login$', data=post_encoded, method='POST')
+  req = urllib.request.Request(r'^localhost:8001/api/v1/users/create$', data=post_encoded, method='POST')
   resp_json = urllib.request.urlopen(req).read().decode('utf-8')
   resp = json.loads(resp_json)
   return JsonResponse(resp)
